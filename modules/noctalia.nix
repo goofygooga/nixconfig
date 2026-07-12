d6980042fcf56223145bd346335fbd7da450f977
goofygooga <114880912+goofygooga@users.noreply.github.com>
2026-07-11 23:38:31 -0400

    Configure xdg.portal with additional settings
    Added xdg.portal configuration for enabling extra portals.

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -5,6 +5,14 @@
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     # ... maybe other stuff
   ];
+xdg.portal = {
+  enable = true;
+  extraPortals = with pkgs; [
+    xdg-desktop-portal-gtk  # Needed for file pickers
+    xdg-desktop-portal-gnome
+    # kdePackages.xdg-desktop-portal-kde # Use if on KDE Plasma
+  ];
+};
 nix.settings = {
   extra-substituters = [ "https://noctalia.cachix.org" ];
   extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
